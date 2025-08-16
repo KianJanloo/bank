817abb04656442a05e4c5c64377ce606a493575c
Kian Janloo <ddrdslayer@gmail.com>
2025-08-17 00:24:44 +0330

    Resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
@@ -15,6 +19,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    AuthModule,
+    UsersModule,
+    AccountsModule,
+    TransactionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
